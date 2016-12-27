bba89599297ff830201f9e2b79083b99f9b7ac2a
Kevin Purnelle <kevin.purnelle@gmail.com>
2016-12-27 01:40:05 +0100

    feat(app): load styles

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import reactDom from 'react-dom'
 import App from './App'
+import styles from './styles/index.less' // eslint-disable-line no-unused-vars
 
 reactDom.render(
   <App />,
