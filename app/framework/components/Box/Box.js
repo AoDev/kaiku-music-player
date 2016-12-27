43be874ffb0e4fd1d10be6d5834037fa60b4490d
Kevin Purnelle <kevin.purnelle@gmail.com>
2016-12-27 11:45:13 +0100

    feat(framework): new Box component

---
+++ app/framework/components/Box/Box.js
@@ -0,0 +1,34 @@
+import React, {Component, PropTypes} from 'react'
+
+export default class Box extends Component {
+  render () {
+    const {hidden, visible, fullHeight} = this.props
+    const style = {}
+    var className = null
+
+    if (hidden || visible === false) {
+      className = 'hidden'
+    }
+
+    if (fullHeight) {
+      style.height = '100%'
+    }
+
+    return (
+      <div className={className} style={style}>
+        {this.props.children}
+      </div>
+    )
+  }
+}
+
+Box.propTypes = {
+  hidden: PropTypes.bool,
+  visible: PropTypes.bool,
+  fullHeight: PropTypes.bool,
+  children: PropTypes.node
+}
+
+Box.defaultProps = {
+  fullHeight: false
+}
