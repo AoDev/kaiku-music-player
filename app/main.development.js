839dc632cd52c0c13da7df5fade3f3848d378dda
Kevin Purnelle <kevin.purnelle@gmail.com>
2016-12-27 11:50:40 +0100

    feat(main.js development): add menu for OSX

--- app/main.development.js
+++ app/main.development.js
@@ -1,274 +1,137 @@
-import { app, BrowserWindow, Menu, shell } from 'electron';
+import {app, BrowserWindow, Menu, globalShortcut} from 'electron'
+import windowMenu from './windowMenu'
 
-let menu;
-let template;
-let mainWindow = null;
+let menu
+let mainWindow = null
 
 if (process.env.NODE_ENV === 'production') {
-  const sourceMapSupport = require('source-map-support'); // eslint-disable-line
-  sourceMapSupport.install();
+  const sourceMapSupport = require('source-map-support') // eslint-disable-line
+  sourceMapSupport.install()
 }
 
 if (process.env.NODE_ENV === 'development') {
-  require('electron-debug')(); // eslint-disable-line global-require
-  const path = require('path'); // eslint-disable-line
-  const p = path.join(__dirname, '..', 'app', 'node_modules'); // eslint-disable-line
-  require('module').globalPaths.push(p); // eslint-disable-line
+  require('electron-debug')() // eslint-disable-line global-require
+  const path = require('path') // eslint-disable-line
+  const p = path.join(__dirname, '..', 'app', 'node_modules') // eslint-disable-line
+  require('module').globalPaths.push(p) // eslint-disable-line
 }
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit();
-});
-
-
-const installExtensions = async () => {
-  if (process.env.NODE_ENV === 'development') {
-    const installer = require('electron-devtools-installer'); // eslint-disable-line global-require
-
-    const extensions = [
-      'REACT_DEVELOPER_TOOLS',
-      'REDUX_DEVTOOLS'
-    ];
-    const forceDownload = !!process.env.UPGRADE_EXTENSIONS;
-    for (const name of extensions) { // eslint-disable-line
-      try {
-        await installer.default(installer[name], forceDownload);
-      } catch (e) {} // eslint-disable-line
-    }
+  if (process.platform !== 'darwin') {
+    app.quit()
   }
-};
+})
+
+// const installExtensions = async () => {
+//   if (process.env.NODE_ENV === 'development') {
+//     const installer = require('electron-devtools-installer') // eslint-disable-line global-require
+
+//     const extensions = [
+//       'REACT_DEVELOPER_TOOLS',
+//       'REDUX_DEVTOOLS'
+//     ]
+//     const forceDownload = !!process.env.UPGRADE_EXTENSIONS
+//     for (const name of extensions) { // eslint-disable-line
+//       try {
+//         await installer.default(installer[name], forceDownload)
+//       } catch (e) {} // eslint-disable-line
+//     }
+//   }
+// }
 
 app.on('ready', async () => {
-  await installExtensions();
+  // await installExtensions()
 
   mainWindow = new BrowserWindow({
     show: false,
     width: 1024,
     height: 728
-  });
+  })
 
-  mainWindow.loadURL(`file://${__dirname}/app.html`);
+  mainWindow.loadURL(`file://${__dirname}/app.html`)
 
   mainWindow.webContents.on('did-finish-load', () => {
-    mainWindow.show();
-    mainWindow.focus();
-  });
+    mainWindow.show()
+    mainWindow.focus()
+  })
 
   mainWindow.on('closed', () => {
-    mainWindow = null;
-  });
+    globalShortcut.unregisterAll()
+    mainWindow = null
+  })
 
   if (process.env.NODE_ENV === 'development') {
-    mainWindow.openDevTools();
-    mainWindow.webContents.on('context-menu', (e, props) => {
-      const { x, y } = props;
+    // mainWindow.webContents.on('context-menu', (e, props) => {
+    //   const { x, y } = props
 
-      Menu.buildFromTemplate([{
-        label: 'Inspect element',
-        click() {
-          mainWindow.inspectElement(x, y);
-        }
-      }]).popup(mainWindow);
-    });
+    //   Menu.buildFromTemplate([{
+    //     label: 'Inspect element',
+    //     click () {
+    //       mainWindow.inspectElement(x, y)
+    //     }
+    //   }]).popup(mainWindow)
+    // })
   }
 
   if (process.platform === 'darwin') {
-    template = [{
-      label: 'Electron',
-      submenu: [{
-        label: 'About ElectronReact',
-        selector: 'orderFrontStandardAboutPanel:'
-      }, {
-        type: 'separator'
-      }, {
-        label: 'Services',
-        submenu: []
-      }, {
-        type: 'separator'
-      }, {
-        label: 'Hide ElectronReact',
-        accelerator: 'Command+H',
-        selector: 'hide:'
-      }, {
-        label: 'Hide Others',
-        accelerator: 'Command+Shift+H',
-        selector: 'hideOtherApplications:'
-      }, {
-        label: 'Show All',
-        selector: 'unhideAllApplications:'
-      }, {
-        type: 'separator'
-      }, {
-        label: 'Quit',
-        accelerator: 'Command+Q',
-        click() {
-          app.quit();
-        }
-      }]
-    }, {
-      label: 'Edit',
-      submenu: [{
-        label: 'Undo',
-        accelerator: 'Command+Z',
-        selector: 'undo:'
-      }, {
-        label: 'Redo',
-        accelerator: 'Shift+Command+Z',
-        selector: 'redo:'
-      }, {
-        type: 'separator'
-      }, {
-        label: 'Cut',
-        accelerator: 'Command+X',
-        selector: 'cut:'
-      }, {
-        label: 'Copy',
-        accelerator: 'Command+C',
-        selector: 'copy:'
-      }, {
-        label: 'Paste',
-        accelerator: 'Command+V',
-        selector: 'paste:'
-      }, {
-        label: 'Select All',
-        accelerator: 'Command+A',
-        selector: 'selectAll:'
-      }]
-    }, {
-      label: 'View',
-      submenu: (process.env.NODE_ENV === 'development') ? [{
-        label: 'Reload',
-        accelerator: 'Command+R',
-        click() {
-          mainWindow.webContents.reload();
-        }
-      }, {
-        label: 'Toggle Full Screen',
-        accelerator: 'Ctrl+Command+F',
-        click() {
-          mainWindow.setFullScreen(!mainWindow.isFullScreen());
-        }
-      }, {
-        label: 'Toggle Developer Tools',
-        accelerator: 'Alt+Command+I',
-        click() {
-          mainWindow.toggleDevTools();
-        }
-      }] : [{
-        label: 'Toggle Full Screen',
-        accelerator: 'Ctrl+Command+F',
-        click() {
-          mainWindow.setFullScreen(!mainWindow.isFullScreen());
-        }
-      }]
-    }, {
-      label: 'Window',
-      submenu: [{
-        label: 'Minimize',
-        accelerator: 'Command+M',
-        selector: 'performMiniaturize:'
-      }, {
-        label: 'Close',
-        accelerator: 'Command+W',
-        selector: 'performClose:'
-      }, {
-        type: 'separator'
-      }, {
-        label: 'Bring All to Front',
-        selector: 'arrangeInFront:'
-      }]
-    }, {
-      label: 'Help',
-      submenu: [{
-        label: 'Learn More',
-        click() {
-          shell.openExternal('http://electron.atom.io');
-        }
-      }, {
-        label: 'Documentation',
-        click() {
-          shell.openExternal('https://github.com/atom/electron/tree/master/docs#readme');
-        }
-      }, {
-        label: 'Community Discussions',
-        click() {
-          shell.openExternal('https://discuss.atom.io/c/electron');
-        }
-      }, {
-        label: 'Search Issues',
-        click() {
-          shell.openExternal('https://github.com/atom/electron/issues');
-        }
-      }]
-    }];
-
-    menu = Menu.buildFromTemplate(template);
-    Menu.setApplicationMenu(menu);
-  } else {
-    template = [{
-      label: '&File',
-      submenu: [{
-        label: '&Open',
-        accelerator: 'Ctrl+O'
-      }, {
-        label: '&Close',
-        accelerator: 'Ctrl+W',
-        click() {
-          mainWindow.close();
-        }
-      }]
-    }, {
-      label: '&View',
-      submenu: (process.env.NODE_ENV === 'development') ? [{
-        label: '&Reload',
-        accelerator: 'Ctrl+R',
-        click() {
-          mainWindow.webContents.reload();
-        }
-      }, {
-        label: 'Toggle &Full Screen',
-        accelerator: 'F11',
-        click() {
-          mainWindow.setFullScreen(!mainWindow.isFullScreen());
-        }
-      }, {
-        label: 'Toggle &Developer Tools',
-        accelerator: 'Alt+Ctrl+I',
-        click() {
-          mainWindow.toggleDevTools();
-        }
-      }] : [{
-        label: 'Toggle &Full Screen',
-        accelerator: 'F11',
-        click() {
-          mainWindow.setFullScreen(!mainWindow.isFullScreen());
-        }
-      }]
-    }, {
-      label: 'Help',
-      submenu: [{
-        label: 'Learn More',
-        click() {
-          shell.openExternal('http://electron.atom.io');
-        }
-      }, {
-        label: 'Documentation',
-        click() {
-          shell.openExternal('https://github.com/atom/electron/tree/master/docs#readme');
-        }
-      }, {
-        label: 'Community Discussions',
-        click() {
-          shell.openExternal('https://discuss.atom.io/c/electron');
-        }
-      }, {
-        label: 'Search Issues',
-        click() {
-          shell.openExternal('https://github.com/atom/electron/issues');
-        }
-      }]
-    }];
-    menu = Menu.buildFromTemplate(template);
-    mainWindow.setMenu(menu);
+    menu = Menu.buildFromTemplate(windowMenu.osx.createTemplate(mainWindow))
+    Menu.setApplicationMenu(menu)
   }
-});
+  else {
+    menu = Menu.buildFromTemplate(windowMenu.default.createTemplate(mainWindow))
+    mainWindow.setMenu(menu)
+  }
+
+  // Global shortcuts
+  globalShortcut.register('MediaPlayPause', function () {
+    mainWindow.webContents.send('playPause')
+  })
+
+  globalShortcut.register('MediaNextTrack', function () {
+    mainWindow.webContents.send('NextInPlaylist')
+  })
+
+  globalShortcut.register('MediaPreviousTrack', function () {
+    mainWindow.webContents.send('PrevInPlaylist')
+  })
+
+  registerLocalEvents(mainWindow)
+
+  mainWindow.on('focus', function () {
+    registerLocalEvents(mainWindow)
+  })
+
+  mainWindow.on('blur', unregisterLocalEvents)
+})
+
+/**
+ * Register events listeners that should work only when the app is focused
+ * @param  {browser window} win
+ */
+function registerLocalEvents (win) {
+  globalShortcut.register('VolumeUp', function () {
+    win.webContents.send('VolumeUp')
+  })
+
+  globalShortcut.register('VolumeDown', function () {
+    win.webContents.send('VolumeDown')
+  })
+
+  globalShortcut.register('VolumeMute', function () {
+    win.webContents.send('VolumeMute')
+  })
+
+  globalShortcut.register('CommandOrControl+F', function () {
+    win.webContents.send('search')
+  })
+}
+
+/**
+ * Unregister events listeners that should work only when app is focused
+ */
+function unregisterLocalEvents () {
+  globalShortcut.unregister('VolumeUp')
+  globalShortcut.unregister('VolumeDown')
+  globalShortcut.unregister('VolumeMute')
+  globalShortcut.unregister('CommandOrControl+F')
+}
